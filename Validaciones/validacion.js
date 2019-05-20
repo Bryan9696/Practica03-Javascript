ab2a63c8025cdc0165c54f131a541a8c764f9661
Bryan Steven Pintado Yunga <Bryan_pin96@hotmail.com>
2019-05-20 12:01:44 -0500

    validar numeros

--- Validaciones/validacion.js
+++ Validaciones/validacion.js
@@ -6,3 +6,22 @@ var valTelefono = false;
 var valDirreccion =   false;
 var valFecha= false;
 var valContrasenia =false;
+var cont = /[0-9]/;
+var n =/[A-Z]{1}[a-z]+\s[A-Z]{1}[a-z]+/;
+
+//funcion que valida el campo de Nombre 
+function validarNumeros(num){   
+   
+    tecla = num.keyCode || num.which;   
+    var nombre = window.document.getElementById("nombres");
+ //verifica que el campo no contenga numeros
+ if(expre.test(nombre.value)==true){
+       
+      nombres.style.borderColor ="red";
+     valNom = false;
+     var aux = nombre.value.substring(nombre.value.length-1);
+     var rempla = nombre.value.replace(aux,"");
+     nombre.value=rempla;
+     
+}
+
