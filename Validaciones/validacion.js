13e8236cce02c068fd5af658a34ae34afda9b3de
Bryan Steven Pintado Yunga <Bryan_pin96@hotmail.com>
2019-05-20 11:57:49 -0500

    Validar cedula

---
+++ Validaciones/validacion.js
@@ -0,0 +1,8 @@
+var valCedula = false;
+var valNombre = false;
+var valApellido = false;
+var valEmail = false;
+var valTelefono = false;
+var valDirreccion =   false;
+var valFecha= false;
+var valContrasenia =false;
